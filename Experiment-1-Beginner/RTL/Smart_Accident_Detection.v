60740a4b15317694b530b9137b7e1877a302ce64
lakshmisrinivasramagiri-creator <lakshmisrinivasramagiri@gmail.com>
2026-09-09 09:55:39 +0530

    Update Smart_Accident_Detection.v

--- Experiment-1-Beginner/RTL/Smart_Accident_Detection.v
+++ Experiment-1-Beginner/RTL/Smart_Accident_Detection.v
@@ -1,1 +1,14 @@
+module Smart_Accident_Detection (
+    input  wire motion,
+    input  wire impact,
+    input  wire tilt,
+    output wire motion_led,
+    output wire impact_led,
+    output wire accident_led
+);
 
+    assign motion_led   = motion;
+    assign impact_led   = impact;
+    assign accident_led = impact & tilt;
+
+endmodule
